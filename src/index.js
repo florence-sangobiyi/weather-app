d285744d6cecffe6ef282d3d32adf9ac16840e32
Titilayo Sangobiyi <florencetitilayk@gmail.com>
2019-11-24 17:16:12 +0100

    handle invalid reply

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 const readline = require('readline')
 
-const {fetchWeatherInfo} = require('./helpers/axioxUtils')
+const { fetchWeatherInfo } = require('./helpers/axioxUtils')
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 })
 
 rl.question('What city weather do you want to check for? ', (answer) => {
-    if (answer === '') {
-        console.log('Sorry! you have to provide a city')
-        rl.close()
-    } else {
-        fetchWeatherInfo(answer, (data) => {
-            console.log(`The temperature in ${answer} is ${(data.main.temp - 273.15)} degrees celsius.`)
-            console.log('Have a good day')
-            rl.close()
-        })
-    }
+  if (answer === '') {
+    console.log('Sorry! you have to provide a city')
+    rl.close()
+  } else {
+    fetchWeatherInfo(answer, (data) => {
+      if (data.main) {
+        console.log(`The temperature in ${answer} is ${(data.main.temp - 273.15)} degrees celsius.`)
+        console.log('Have a good day')
+      } else {
+        console.log(`There was an error when trying to fetch data for ${answer}`)
+      }
+
+      rl.close()
+    })
+  }
 })
 
 
